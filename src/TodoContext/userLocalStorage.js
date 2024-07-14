2cc986101d0b1e5f9c54823e097bc3a3e44f3c91
Rigoleto723 <ivanvargasc723@gmail.com>
2024-07-14 14:01:52 -0500

    correccion warning en useEffect

--- src/TodoContext/userLocalStorage.js
+++ src/TodoContext/userLocalStorage.js
@@ -30,7 +30,7 @@ function useLocalStorage(itemName, initialValue) {
         setError(true);
       }
     }, 2000);
-  }, []);
+  });
 
   const saveItem = (newItem) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
